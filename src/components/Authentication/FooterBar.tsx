47b4680f807c85e2a0a9f73462f0267a828d81d1
devdgna <dev.dgna@gmail.com>
2026-02-04 11:36:38 +0600

    fix: add space between year and company name in footers

--- src/components/Authentication/FooterBar.tsx
+++ src/components/Authentication/FooterBar.tsx
@@ -5,7 +5,7 @@ const FooterBar = () => {
         <footer className="bg-white dark:bg-gray-800">
             <div className="md:flex md:items-center md:justify-between p-3">
                 <p className="text-sm text-center text-gray-500">
-                    &copy; 2019 - {new Date().getFullYear()} 
+                    &copy; 2019 - {new Date().getFullYear()}  
                     <a className="hover:underline" target="_blank"
                     >{envConfig.PLATFORM_DATA.poweredBy}
                     </a> | All rights reserved.
