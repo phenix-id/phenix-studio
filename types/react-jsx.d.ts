cb2b21ff0215eeced3b737c771710b239cc33515
devdgna <dev.dgna@gmail.com>
2026-06-04 14:07:01 +0600

    fix: restore global JSX namespace by mapping it to React 19's React.JSX namespace
    Signed-off-by: devdgna <dev.dgna@gmail.com>

---
+++ types/react-jsx.d.ts
@@ -0,0 +1,24 @@
+import type * as React from 'react'
+
+declare global {
+  namespace JSX {
+    type ElementType = React.JSX.ElementType
+    type LibraryManagedAttributes<C, P> = React.JSX.LibraryManagedAttributes<
+      C,
+      P
+    >
+
+    interface Element extends React.JSX.Element {}
+    interface ElementClass extends React.JSX.ElementClass {}
+    interface ElementAttributesProperty
+      extends React.JSX.ElementAttributesProperty {}
+    interface ElementChildrenAttribute
+      extends React.JSX.ElementChildrenAttribute {}
+    interface IntrinsicAttributes extends React.JSX.IntrinsicAttributes {}
+    interface IntrinsicClassAttributes<T>
+      extends React.JSX.IntrinsicClassAttributes<T> {}
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
+}
+
+export {}
